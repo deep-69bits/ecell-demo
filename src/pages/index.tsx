50156a684f136dc491c186b4c33f4bcd64deb3cf
KSJ <kuldeepjhala03@gmail.com>
2023-06-03 23:00:39 +0530

    about-us template

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,16 @@
 import React from "react"
 
+import AboutUs from "@/components/home/about-us/about-us"
+
+// eslint-disable-next-line @typescript-eslint/no-empty-interface
 interface HomePageProps {}
 
 const HomePage: React.FC<HomePageProps> = () => {
-  return <div>HomePage</div>
+  return (
+    <div>
+      <AboutUs />
+    </div>
+  )
 }
 
 export default HomePage
